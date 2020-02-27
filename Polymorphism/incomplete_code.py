503ee1685a2dcbf16cb0cdfe3c58f3ffff495ade
Bob <janekarnkit2013@hotmail.com>
2020-02-27 17:47:06 +0700

    Refactor

--- Polymorphism/incomplete_code.py
+++ Polymorphism/incomplete_code.py
@@ -1,16 +1,4 @@
-class Transportation(object):
-   """Abstract base class"""
-
-   def __init__( self, start, end, distance ):
-      if self.__class__ == Transportation:
-         raise NotImplementedError
-      self.start = start
-      self.end = end
-      self.distance = distance
-
-   def find_cost( self ):
-      """Abstract method; derived classes must override"""
-      raise NotImplementedError
+import Transportation
 
 
 class Walk( Transportation ):
