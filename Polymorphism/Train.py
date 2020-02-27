a68aa613271dc64d5d1f4a5728e88c11e72cacd1
Bob <janekarnkit2013@hotmail.com>
2020-02-27 18:17:17 +0700

    remove comment

--- Polymorphism/Train.py
+++ Polymorphism/Train.py
@@ -1,10 +1,10 @@
-travel_cost = 0
+import Transportation
 
-#trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         #Train("Ladkrabang Station","Payathai Station",40,6),
-         #Taxi("Payathai Station","The British Council",3) ]
+class Train( Transportation ):
 
-for travel in trip:
-   travel_cost += travel.find_cost()
-print travel_cost
+   def __init__( self, start, end, distance ):
+      Transportation.__init__( self, start, end, distance)
+
+   def find_cost( self ):
+      return 0
+
---
+++ Polymorphism/Transportation.py
@@ -0,0 +1,13 @@
+class Transportation(object):
+   """Abstract base class"""
+
+   def __init__( self, start, end, distance ):
+      if self.__class__ == Transportation:
+         raise NotImplementedError
+      self.start = start
+      self.end = end
+      self.distance = distance
+
+   def find_cost( self ):
+      """Abstract method; derived classes must override"""
+      raise NotImplementedError
